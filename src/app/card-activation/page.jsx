fc5f22449fbf7466735cc0d44473d3932377845b
SergWebAcceptance <serg.web@acceptance.ltd>
2025-05-14 12:53:21 +0300

    added new page

---
+++ src/app/card-activation/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const metadata = {
+    title: "Card Activation | Clarity Solutions",
+    description: "",
+    openGraph: {
+      title: "Card Activation | Clarity Solutions",
+      description: "",
+      images: "https://clarity-solutions.io/images/meta.jpg",
+    },
+  };
+
+const CardActivation = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default CardActivation
