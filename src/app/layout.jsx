336fb6002704e161bd7633f98c116223ca5ddfae
SergWebAcceptance <serg.web@acceptance.ltd>
2025-07-17 18:44:21 +0300

    Clear metadata descriptions in layout component for improved clarity and focus.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,23 +9,6 @@ import DeviceRedirect from "../components/DeviceRedirect";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-export const metadata = {
-  title: {
-    default: "Flexible Payment Solutions | Clarity Solutions",
-    template: "%s",
-  },
-  description:
-    "Manage your money securely and unlock financial freedom with Clarity Solutions. Explore our versatile debit card options and seamless payment solutions.",
-  openGraph: {
-    title: {
-      default: "Flexible Payment Solutions | Clarity Solutions",
-      template: "%s",
-    },
-    description:
-      "Manage your money securely and unlock financial freedom with Clarity Solutions. Explore our versatile debit card options and seamless payment solutions.",
-    images: "https://clarity-solutions.io/images/meta.jpg",
-  },
-};
 
 export default function RootLayout({ children }) {
   return (
