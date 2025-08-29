d0ead0036cb80dc6293a057da2e4bb63b130586a
SergWebAcceptance <serg.web@acceptance.ltd>
2025-08-29 17:57:29 +0300

    Update metadata for Card Activation page to enhance SEO with detailed descriptions for better user engagement.

--- src/app/card-activation/page.jsx
+++ src/app/card-activation/page.jsx
@@ -6,10 +6,12 @@ import NeedAssistance from "./components/NeedAssistance/NeedAssistance";
 
 export const metadata = {
   title: "Card Activation | Clarity Solutions",
-  description: "",
+  description:
+    "Learn how to activate your Clarity Solutions debit card quickly and easily. Follow our step-by-step instructions or watch the video guide to get started.",
   openGraph: {
     title: "Card Activation | Clarity Solutions",
-    description: "",
+    description:
+      "Learn how to activate your Clarity Solutions debit card quickly and easily. Follow our step-by-step instructions or watch the video guide to get started.",
     images: "https://clarity-solutions.io/images/meta.jpg",
   },
 };
